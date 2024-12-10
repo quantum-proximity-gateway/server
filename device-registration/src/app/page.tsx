0fc96fcbcab10cfdf3a41ba10f0a9bd4dd6d7d1b
Marwan <yassinimarwan6@gmail.com>
2024-12-10 12:52:44 +0000

    Added first endpoint and env variables

--- device-registration/src/app/page.tsx
+++ device-registration/src/app/page.tsx
@@ -1,13 +1,29 @@
 'use client';
 
 import { Button, Input, Stack } from "@chakra-ui/react";
-import { useState } from "react";
-import '@fontsource/ibm-plex-sans'; // Import IBM Plex Sans font
+import { useState, useEffect } from "react";
+import '@fontsource/ibm-plex-sans';
+import axios from 'axios';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Home() {
   const [macAddress, setMacAddress] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  function handleRegister() {
+    console.log(API_URL);
+    axios.post(`${API_URL}/register`, {
+      macAddress,
+      username,
+      password
+    }).then((res) => {
+      console.log(res.data);
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2" style={{ fontFamily: 'IBM Plex Sans, sans-serif' }}>
@@ -18,7 +34,7 @@ export default function Home() {
         <Input placeholder="MAC Address" variant="flushed" onChange={(e) => setMacAddress(e.target.value)}/>
         <Input placeholder="Username" variant="flushed" onChange={(e) => setUsername(e.target.value)}/>
         <Input placeholder="Password" variant="flushed" onChange={(e) => setPassword(e.target.value)}/>
-        <Button variant="surface" className="mt-4" width="100%">
+        <Button variant="surface" className="mt-4" width="100%" onClick={handleRegister}>
           Register
         </Button>
       </Stack>
