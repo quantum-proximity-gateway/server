4443603da4f84a0448a015f8568fd65474eb6bde
Marwan <yassinimarwan6@gmail.com>
2024-12-10 12:20:51 +0000

    Started the backend

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,12 @@
-import flask
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/register', methods=['POST'])
+def registerDevice():
+    data = request.get_json()
+    print(data)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
