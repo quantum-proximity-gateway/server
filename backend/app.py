265e89b60fc4b6cbbc74f3c3e85d5ac83b2df235
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2024-12-29 08:22:23 +0000

    Created Device and Preferences ORM classes

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,26 @@
-from litestar import Litestar, get
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
-@get('/')
-async def home() -> str:
-    return "Hello World!"
+class Base(DeclarativeBase):
+    pass
 
-app = Litestar([home])
+
+class Device(Base):
+    __tablename__ = 'devices'
+
+    mac_address: Mapped[str] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    password: Mapped[str]
+    key: Mapped[str]
+    preferences: Mapped['Preferences'] = relationship(back_populates='device', uselist=False)
+
+
+class Preferences(Base):
+    __tablename__ = 'preferences'
+
+    mac_address: Mapped[str] = mapped_column(
+        ForeignKey('devices.mac_address', ondelete='CASCADE'),
+        primary_key=True
+    )
+    settings: Mapped[dict]
