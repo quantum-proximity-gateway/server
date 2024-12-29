8668b0efdfc170804d62d243c1f0c0bef3395356
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2024-12-29 17:57:24 +0000

    Fixed register device function

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+from advanced_alchemy.extensions.litestar.plugins.init.config.asyncio import autocommit_before_send_handler
+from collections.abc import AsyncGenerator
 from litestar import Litestar, get, post
 from litestar.plugins.sqlalchemy import SQLAlchemyAsyncConfig, SQLAlchemyPlugin
 from pydantic import BaseModel
@@ -25,34 +27,33 @@ class RegisterDeviceRequest(BaseModel):
     mac_address: str
     username: str
     password: str
-    preferences: str = '{}'
 
 
 def generate_key(length: int = 32) -> str:
     return ''.join(secrets.choice([chr(i) for i in range(0x21, 0x7F)]) for _ in range(length))
 
+async def provide_transaction(db_session: AsyncSession) -> AsyncGenerator[AsyncSession, None]:
+    async with db_session.begin():
+        yield db_session
+
 @get('/devices')
-async def get_devices(db_session: AsyncSession) -> list[Device]:
+async def get_devices(transaction: AsyncSession) -> list[Device]:
     query = select(Device)
-    result = await db_session.execute(query)
+    result = await transaction.execute(query)
     devices = result.scalars().all()
     return devices
 
 @post('/devices')
-async def register_device(data: RegisterDeviceRequest, db_session: AsyncSession) -> Device:
+async def register_device(data: RegisterDeviceRequest, transaction: AsyncSession) -> Device:
     key = generate_key()
-
     device = Device(
         mac_address=data.mac_address,
         username=data.username,
         password=data.password,
         key=key,
-        preferences=data.preferences
+        preferences='{}'
     )
-
-    db_session.add(device)
-    await db_session.commit()
-
+    transaction.add(device)
     return device
 
 # validate key
@@ -63,10 +64,12 @@ async def register_device(data: RegisterDeviceRequest, db_session: AsyncSession)
 
 # update preferences for device
 
+
 db_config = SQLAlchemyAsyncConfig(
     connection_string='sqlite+aiosqlite:///db.sqlite',
     metadata=Base.metadata,
-    create_all=True
+    create_all=True,
+    before_send_handler=autocommit_before_send_handler
 )
 sqlalchemy_plugin = SQLAlchemyPlugin(config=db_config)
 
@@ -75,6 +78,7 @@ app = Litestar(
         get_devices,
         register_device
     ],
+    dependencies={'transaction': provide_transaction},
     plugins=[sqlalchemy_plugin],
     debug=True
 )
