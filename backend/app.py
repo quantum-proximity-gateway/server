ff75b3a7e3ad7e2600d9cbf6afd8b7d6bb148487
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2024-12-29 08:51:30 +0000

    Setup sqlite database

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+from litestar import Litestar
+from litestar.plugins.sqlalchemy import SQLAlchemyAsyncConfig, SQLAlchemyPlugin
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
@@ -23,4 +25,17 @@ class Preferences(Base):
         ForeignKey('devices.mac_address', ondelete='CASCADE'),
         primary_key=True
     )
-    settings: Mapped[dict]
+    settings: Mapped[str]
+
+
+db_config = SQLAlchemyAsyncConfig(
+    connection_string='sqlite+aiosqlite:///db.sqlite',
+    metadata=Base.metadata,
+    create_all=True
+)
+sqlalchemy_plugin = SQLAlchemyPlugin(db_config)
+
+app = Litestar(
+    route_handlers=[],
+    plugins=[sqlalchemy_plugin]
+)
