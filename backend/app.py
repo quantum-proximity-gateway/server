99b25bbfb970c51be7e148f3d1fe7db0d8288e62
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2024-12-29 13:53:39 +0000

    Added generate key function

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
-from litestar import Litestar, get
+from litestar import Litestar, get, post
 from litestar.plugins.sqlalchemy import SQLAlchemyAsyncConfig, SQLAlchemyPlugin
+from pydantic import BaseModel
 from sqlalchemy import ForeignKey, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
+import secrets
 
 
 class Base(DeclarativeBase):
@@ -30,6 +32,15 @@ class Preferences(Base):
     device: Mapped[Device] = relationship(back_populates='preferences')
 
 
+class RegisterDeviceRequest(BaseModel):
+    mac_address: str
+    username: str
+    password: str
+
+
+def generate_key(length: int = 32) -> str:
+    return ''.join(secrets.choice([chr(i) for i in range(0x21, 0x7F)]) for _ in range(length))
+
 @get('/devices')
 async def get_devices(db_session: AsyncSession) -> list[Device]:
     query = select(Device).options(selectinload(Device.preferences))
@@ -37,7 +48,9 @@ async def get_devices(db_session: AsyncSession) -> list[Device]:
     devices = result.scalars().all()
     return devices
 
-# register device
+@post('/devices')
+async def register_device(db_session: AsyncSession, request: RegisterDeviceRequest) -> Device:
+    pass
 
 # validate key
 
@@ -55,7 +68,10 @@ db_config = SQLAlchemyAsyncConfig(
 sqlalchemy_plugin = SQLAlchemyPlugin(config=db_config)
 
 app = Litestar(
-    route_handlers=[get_devices],
+    route_handlers=[
+        get_devices,
+        register_device
+    ],
     plugins=[sqlalchemy_plugin],
     debug=True
 )
