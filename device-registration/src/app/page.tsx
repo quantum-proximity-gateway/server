756fec21e0c356c0d54c5dafa6277bfd72034e9f
Marwan <yassinimarwan6@gmail.com>
2024-12-29 23:10:15 +0100

    Added mac address retrieval via navigator.serial API

--- device-registration/src/app/page.tsx
+++ device-registration/src/app/page.tsx
@@ -6,10 +6,50 @@ import '@fontsource/ibm-plex-sans';
 import axios from 'axios';
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+
+async function connectSerial() { // Connect to ESP32 (cu.wchuusbserial)
+  const log = document.getElementById('target');
+    
+  try {
+    const port = await navigator.serial.requestPort();
+    await port.open({ baudRate: 9600 });
+    
+    const decoder = new TextDecoderStream(); // Decodes incoming data from ESP32
+    
+    port.readable.pipeTo(decoder.writable);
+
+    const inputStream = decoder.readable;
+    const reader = inputStream.getReader();
+    let macAddress = "";
+    while (true) {
+      const { value, done } = await reader.read();
+      if (value) {
+        console.log('[readLoop] value:', value, "length:", value.length);
+        if (value.length == 19) { // MAC Address are 17 characters long + 2 newlines
+          macAddress = value;
+          reader.releaseLock();
+          break;
+        }
+      }
+      if (done) {
+        console.log('[readLoop] DONE', done);
+        reader.releaseLock();
+        break;
+      }
+    }
+    return macAddress;
+
+  
+  } catch (error) {
+    console.error('There was an error reading the data:', error);
+  }
+}
+
 export default function Home() {
   const [macAddress, setMacAddress] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [connected, setConnected] = useState(false);
 
   function handleRegister() {
     axios.post(`${API_URL}/register`, {
@@ -24,6 +64,19 @@ export default function Home() {
     });
   }
 
+  function handleConnect() {
+    if (navigator.serial) {
+      connectSerial().then(address => {
+        if (address) {
+          setMacAddress(address);
+          setConnected(true);
+        }
+      });
+    } else {
+      alert("Web Serial API not supported in this browser, install the latest version of Chrome or Edge");
+    }
+  }
+
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2" style={{ fontFamily: 'IBM Plex Sans, sans-serif' }}>
@@ -31,7 +84,10 @@ export default function Home() {
         Device Registration
       </h1>
       <Stack className="sm:p-20">
-        <Input placeholder="MAC Address" variant="flushed" onChange={(e) => setMacAddress(e.target.value)}/>
+        <Input placeholder="MAC Address" variant="flushed" value={macAddress} readOnly/>
+        {!connected && (<Button variant="surface" width="100%" onClick={handleConnect}>
+          Connect to device
+        </Button>)}
         <Input placeholder="Username" variant="flushed" onChange={(e) => setUsername(e.target.value)}/>
         <Input placeholder="Password" variant="flushed" onChange={(e) => setPassword(e.target.value)}/>
         <Button variant="surface" className="mt-4" width="100%" onClick={handleRegister}>
