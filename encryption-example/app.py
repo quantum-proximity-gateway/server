b89c120621c82130698334df353b41446ed345a7
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-02-28 14:57:22 +0000

    Complete the KEM initiate endpoint

--- encryption-example/app.py
+++ encryption-example/app.py
@@ -1,11 +1,18 @@
 from litestar import Litestar, get, post
+from litestar.exceptions import HTTPException
 from pydantic import BaseModel
 import oqs
 
 KEM_ALGORITHM = 'Kyber512'
 
+kem_sessions = {}
+
 
 class KEMInitiateRequest(BaseModel):
+    rpi_id: str
+
+
+class KEMCompleteRequest(BaseModel):
     rpi_id: str
 
 
@@ -15,8 +22,20 @@ def kem_initiate(data: KEMInitiateRequest) -> dict:
     Initiate key exchange session. The server generates a KEM key pair and returns the public key.
     '''
 
-    
+    server_kem = oqs.KeyEncapsulation(KEM_ALGORITHM)
+    kem_sessions[data.rpi_id] = server_kem
+    public_key = server_kem.generate_keypair()
+    return {'public_key': public_key}
 
+@post('/kem/complete')
+def kem_complete(data: KEMCompleteRequest) -> dict:
+    '''
+    Complete the key exchange. The client sends back the encapsulated shared secret that they have generated.
+    '''
+
+    server_kem = kem_sessions.pop(data.rpi_id, None)
+    if not server_kem:
+        ...
 
 app = Litestar(
     route_handlers=[
--- encryption-example/rpi_client.py
+++ encryption-example/rpi_client.py
@@ -1,4 +1,4 @@
-from oqs import oqs_python_version, oqs_version
+from oqs import oqs_python_version, oqs_version, get_enabled_kem_mechanisms
 
 
-print(f'{oqs_python_version()}, {oqs_version()}')
+print(f'{oqs_python_version()}, {oqs_version()}, {get_enabled_kem_mechanisms()}')
