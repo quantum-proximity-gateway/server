657d8cfbfc9326ef2ee03dca8710e9bff889f235
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-03-01 10:42:15 +0000

    Added try-excepts to rpi code

--- encryption-example/rpi_client.py
+++ encryption-example/rpi_client.py
@@ -1,6 +1,7 @@
 import requests
 import oqs
 import base64
+from aesgcm_encryption import aesgcm_encrypt, aesgcm_decrypt
 
 CLIENT_ID = 42
 KEM_ALGORITHM = 'Kyber512'
@@ -23,19 +24,27 @@ def generate_shared_secret() -> bytes:
     response = requests.post(f'{SERVER_URL}/kem/initiate', json=data)
     public_key_b64 = response.json().get('public_key_b64')
     if not public_key_b64:
-        raise KEMException('Public key not found in response')
-    public_key = base64.b64decode(public_key_b64)
+        raise KEMException('Public key not found in response.')
 
     # Encapsulate a shared secret
-    ciphertext, shared_secret = client_kem.encap_secret(public_key)
+    try:
+        public_key = base64.b64decode(public_key_b64)
+        ciphertext, shared_secret = client_kem.encap_secret(public_key)
+    except Exception as e:
+        raise KEMException('Invalid public key, could not encapsulate secret.')
 
     # Send encapsulated shared secret
     ciphertext_b64 = base64.b64encode(ciphertext).decode()
     data = {'rpi_id': f'{CLIENT_ID}', 'ciphertext_b64': ciphertext_b64}
     response = requests.post(f'{SERVER_URL}/kem/complete', json=data)
     if response.status_code != 201:
-        raise KEMException(f'Unexpected status code: {response.status_code}')
+        raise KEMException(f'Unexpected status code: {response.status_code}.')
     
     return shared_secret
 
-shared_secret = generate_shared_secret()
+if __name__ == '__main__':
+    # Example usage
+    try:
+        shared_secret = generate_shared_secret()
+    except KEMException as e:
+        print(f'KEM failed, could not generate secret: {e}')
