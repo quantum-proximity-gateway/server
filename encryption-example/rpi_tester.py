a78bae1e701542d62177b0ee5551ad546f1d638f
Marwan <yassinimarwan6@gmail.com>
2025-03-02 13:05:01 +0000

    Added tests for the encrypted endpoints

--- encryption-example/rpi_tester.py
+++ encryption-example/rpi_tester.py
@@ -5,14 +5,39 @@ import requests
 if __name__ == "__main__":
     server_url = "http://127.0.0.1:8000"
     encryption_client = EncryptionClient(server_url)
+    macs = []
+
+    # Testing get-all-usernames
     try:
         response = requests.get(f"{server_url}/devices/all-mac-addresses", params={'client_id': encryption_client.CLIENT_ID})
         response.raise_for_status()
         response_json = response.json()
         data = encryption_client.decrypt_request(response_json)
 
-        print([mac.strip() for mac in data['mac_addresses']])
-
+        macs = [mac.strip() for mac in data['mac_addresses']]
+        print(macs)
     except requests.exceptions.RequestException as e:
         print(f"Error occurred while fetching MAC addresses: {e}")
+
+    # Testing get-username
+    try:
+        response = requests.get(f"{server_url}/devices/{macs[0]}/username", params={'client_id': encryption_client.CLIENT_ID})
+        response.raise_for_status()
+        data = encryption_client.decrypt_request(response.json())
+
+        print(data.get("username", "invalid"))
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching username for MAC address {macs[0]}: {e}")
+
+    # Testing get-credentials
+    try:
+        response = requests.get(f"{server_url}/devices/{macs[0]}/credentials", params={'client_id': encryption_client.CLIENT_ID})
+        response.raise_for_status()
+        data = encryption_client.decrypt_request(response.json())
+
+        print(data.get("username", "invalid"), data.get("password", "invalid"))
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching username for MAC address {macs[0]}: {e}")
 
