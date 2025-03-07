4524943492556026e9b39550e4b440be6565eb1c
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-03-07 10:06:09 +0000

    Minor change

--- llama-cpp-example/main.py
+++ llama-cpp-example/main.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     model_path = 'models/ibm-granite_granite-3.2-8b-instruct-Q6_K_L.gguf'
     granite_3_2 = LLM(model_path=model_path)
 
-    instruction = 'You are an assistant that only replies in JSON format with the "message".'
+    instruction = 'You are an assistant that only replies in JSON format with the key "message".'
     granite_3_2.set_custom_instruction(instruction)
 
     prompt = 'Hello, how are you doing?'
