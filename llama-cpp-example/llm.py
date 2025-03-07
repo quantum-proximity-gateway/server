3ead1c8da36550c877386678aeaeca1a9153db58
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-03-07 09:35:19 +0000

    Created LLM class

---
+++ llama-cpp-example/llm.py
@@ -0,0 +1,32 @@
+from llama_cpp import Llama
+
+
+class LLM:
+    def __init__(self, model_path: str) -> None:
+        '''
+        Initialise the model given by the model_path string.
+        '''
+        self.llm = Llama(
+            model_path=model_path,
+        )
+        self.custom_instruction = None
+
+    def set_custom_instruction(self, instruction: str | None) -> None:
+        '''
+        Set custom instruction which is prepended to each prompt.
+        '''
+        self.custom_instruction = instruction
+
+    def generate_response(self, prompt: str) -> str:
+        '''
+        Generate a response for a given prompt.
+        '''
+        if self.custom_instruction:
+            prompt = f'{self.custom_instruction}\n\n{prompt}'
+
+        output = self.llm(
+            prompt,
+            max_tokens=50,
+        )
+
+        return output['choices'][0]['text']
