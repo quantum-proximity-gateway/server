f77a73e205354a3a480448f6b5d7c25a95f5ffc9
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-03-09 14:50:56 +0000

    Remove test_db on start

--- backend/test.py
+++ backend/test.py
@@ -4,7 +4,12 @@ from litestar.testing import AsyncTestClient
 from app import app, encryption_helper
 import pytest
 import pytest_asyncio
+import os
 
+
+TEST_DB_FILENAME = 'test_db.sqlite'
+if os.path.isfile(TEST_DB_FILENAME):
+    os.remove(TEST_DB_FILENAME)
 
 TEST_CLIENT_ID = '1'
 TEST_SHARED_SECRET = b'\xbd\xb4\xe9\xf7\x91\xf3\x97\x90\xc1\x93i\xe2\xc9\x0b\xa3\x115\xac\xcb<\xae\x96\xd6\x16\x88\x18\xc8\xd9FRG?'
@@ -16,10 +21,3 @@ async def test_client() -> AsyncIterator[AsyncTestClient[Litestar]]:
     async with AsyncTestClient(app=app) as client:
         yield client
 
-@pytest.mark.asyncio
-async def test_get_devices(test_client: AsyncTestClient[Litestar]) -> None:
-    {'mac_address': '00:11:22:33:44:55', 'username': 'jdoe', 'password': 'password'}
-    await test_client.post('/register')
-
-    response = await test_client.get(f'/devices?client_id={TEST_CLIENT_ID}')
-    print(response)
