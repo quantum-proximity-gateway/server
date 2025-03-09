e0026d293f29a2162e2b7fdcda5324e7191765ac
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-03-09 15:12:08 +0000

    Test get devices endpoint

--- backend/test.py
+++ backend/test.py
@@ -1,7 +1,7 @@
 from collections.abc import AsyncIterator
 from litestar import Litestar
 from litestar.testing import AsyncTestClient
-from app import app, encryption_helper
+from app import app, encryption_helper, EncryptedMessageRequest
 import pytest
 import pytest_asyncio
 import os
@@ -11,9 +11,13 @@ TEST_DB_FILENAME = 'test_db.sqlite'
 if os.path.isfile(TEST_DB_FILENAME):
     os.remove(TEST_DB_FILENAME)
 
-TEST_CLIENT_ID = '1'
-TEST_SHARED_SECRET = b'\xbd\xb4\xe9\xf7\x91\xf3\x97\x90\xc1\x93i\xe2\xc9\x0b\xa3\x115\xac\xcb<\xae\x96\xd6\x16\x88\x18\xc8\xd9FRG?'
-encryption_helper.shared_secrets[TEST_CLIENT_ID] = TEST_SHARED_SECRET
+TEST_CLIENT_ID_1 = '1'
+TEST_SHARED_SECRET_1 = b'\xbd\xb4\xe9\xf7\x91\xf3\x97\x90\xc1\x93i\xe2\xc9\x0b\xa3\x115\xac\xcb<\xae\x96\xd6\x16\x88\x18\xc8\xd9FRG?'
+encryption_helper.shared_secrets[TEST_CLIENT_ID_1] = TEST_SHARED_SECRET_1
+
+TEST_CLIENT_ID_2 = '2'
+TEST_SHARED_SECRET_2 = b'\xbd\xb4\xe9\xf7\x91\xf3\x97\x90\xc1\x93i\xe2\xc9\x0b\xa3\x115\xac\xcb<\xae\x96\xd6\x16\x88\x18\xc8\xd9FRG?'
+encryption_helper.shared_secrets[TEST_CLIENT_ID_2] = TEST_SHARED_SECRET_2
 
 
 @pytest_asyncio.fixture(scope='function')
@@ -21,3 +25,38 @@ async def test_client() -> AsyncIterator[AsyncTestClient[Litestar]]:
     async with AsyncTestClient(app=app) as client:
         yield client
 
+@pytest.mark.asyncio
+async def test_get_devices(test_client: AsyncTestClient[Litestar]) -> None:
+    # Ensure no devices are returned when none have been registered yet
+    response = await test_client.get(f'/devices?client_id={TEST_CLIENT_ID_1}')
+    response_data = encryption_helper.decrypt_msg(EncryptedMessageRequest(**(response.json() | {'client_id': TEST_CLIENT_ID_1})))
+    assert len(response_data['devices']) == 0
+
+    # Test that one device is returned when a device has been registered
+    data = {
+        'mac_address': '00:11:22:33:44:55',
+        'username': 'john_doe',
+        'password': 'password'
+    }
+    encrypted_data = encryption_helper.encrypt_msg(data, TEST_CLIENT_ID_1)
+    encrypted_data |= {'client_id': TEST_CLIENT_ID_1}
+    response = await test_client.post('/register', json=encrypted_data)
+    assert response.status_code == 201
+
+    response = await test_client.get(f'/devices?client_id={TEST_CLIENT_ID_1}')
+    response_data = encryption_helper.decrypt_msg(EncryptedMessageRequest(**(response.json() | {'client_id': TEST_CLIENT_ID_1})))
+    assert len(response_data['devices']) == 1
+
+    # Test that two devices are returned when two devices have been registered
+    data = {
+        'mac_address': '11:22:33:44:55:66',
+        'username': 'a.',
+        'password': 'password'
+    }
+    encrypted_data = encryption_helper.encrypt_msg(data, TEST_CLIENT_ID_2)
+    encrypted_data |= {'client_id': TEST_CLIENT_ID_2}
+    response = await test_client.post('/register', json=encrypted_data)
+
+    response = await test_client.get(f'/devices?client_id={TEST_CLIENT_ID_1}')
+    response_data = encryption_helper.decrypt_msg(EncryptedMessageRequest(**(response.json() | {'client_id': TEST_CLIENT_ID_1})))
+    assert len(response_data['devices']) == 2
