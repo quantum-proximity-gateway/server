024793c1c2e6f7e0fbe0979b1b8e2beca988e5aa
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-03-09 15:22:37 +0000

    Added test for register device

--- backend/test.py
+++ backend/test.py
@@ -8,8 +8,6 @@ import os
 
 
 TEST_DB_FILENAME = 'test_db.sqlite'
-if os.path.isfile(TEST_DB_FILENAME):
-    os.remove(TEST_DB_FILENAME)
 
 TEST_CLIENT_ID_1 = '1'
 TEST_SHARED_SECRET_1 = b'\xbd\xb4\xe9\xf7\x91\xf3\x97\x90\xc1\x93i\xe2\xc9\x0b\xa3\x115\xac\xcb<\xae\x96\xd6\x16\x88\x18\xc8\xd9FRG?'
@@ -22,6 +20,10 @@ encryption_helper.shared_secrets[TEST_CLIENT_ID_2] = TEST_SHARED_SECRET_2
 
 @pytest_asyncio.fixture(scope='function')
 async def test_client() -> AsyncIterator[AsyncTestClient[Litestar]]:
+    # Delete test database, if it exists, before next test 
+    if os.path.isfile(TEST_DB_FILENAME):
+        os.remove(TEST_DB_FILENAME)
+
     async with AsyncTestClient(app=app) as client:
         yield client
 
@@ -38,13 +40,12 @@ async def test_get_devices(test_client: AsyncTestClient[Litestar]) -> None:
         'username': 'john_doe',
         'password': 'password'
     }
-    encrypted_data = encryption_helper.encrypt_msg(data, TEST_CLIENT_ID_1)
-    encrypted_data |= {'client_id': TEST_CLIENT_ID_1}
+    encrypted_data = {'client_id': TEST_CLIENT_ID_1} | encryption_helper.encrypt_msg(data, TEST_CLIENT_ID_1)
     response = await test_client.post('/register', json=encrypted_data)
     assert response.status_code == 201
 
     response = await test_client.get(f'/devices?client_id={TEST_CLIENT_ID_1}')
-    response_data = encryption_helper.decrypt_msg(EncryptedMessageRequest(**(response.json() | {'client_id': TEST_CLIENT_ID_1})))
+    response_data = encryption_helper.decrypt_msg(EncryptedMessageRequest(**({'client_id': TEST_CLIENT_ID_1} | response.json())))
     assert len(response_data['devices']) == 1
 
     # Test that two devices are returned when two devices have been registered
@@ -53,10 +54,25 @@ async def test_get_devices(test_client: AsyncTestClient[Litestar]) -> None:
         'username': 'a.',
         'password': 'password'
     }
-    encrypted_data = encryption_helper.encrypt_msg(data, TEST_CLIENT_ID_2)
-    encrypted_data |= {'client_id': TEST_CLIENT_ID_2}
+    encrypted_data = {'client_id': TEST_CLIENT_ID_2} | encryption_helper.encrypt_msg(data, TEST_CLIENT_ID_2)
     response = await test_client.post('/register', json=encrypted_data)
 
-    response = await test_client.get(f'/devices?client_id={TEST_CLIENT_ID_1}')
-    response_data = encryption_helper.decrypt_msg(EncryptedMessageRequest(**(response.json() | {'client_id': TEST_CLIENT_ID_1})))
+    response = await test_client.get(f'/devices?client_id={TEST_CLIENT_ID_2}')
+    response_data = encryption_helper.decrypt_msg(EncryptedMessageRequest(**({'client_id': TEST_CLIENT_ID_2} | response.json())))
     assert len(response_data['devices']) == 2
+
+@pytest.mark.asyncio
+async def test_register_device(test_client: AsyncTestClient) -> None:
+    # Test registering a new device
+    data = {
+        'mac_address': '00:11:22:33:44:55',
+        'username': 'john_doe',
+        'password': 'password'
+    }
+    encrypted_data = {'client_id': TEST_CLIENT_ID_1} | encryption_helper.encrypt_msg(data, TEST_CLIENT_ID_1)
+    response = await test_client.post('/register', json=encrypted_data)
+    assert response.status_code == 201
+
+    # Test registering the same device again (should fail)
+    response = await test_client.post('/register', json=encrypted_data)
+    assert response.status_code == 409
