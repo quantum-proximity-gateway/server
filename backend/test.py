d421c5c8c052952b73ff6137801e547798433f3a
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-03-09 12:15:39 +0000

    Added shared secret

--- backend/test.py
+++ backend/test.py
@@ -1,27 +1,14 @@
-from collections.abc import AsyncIterator, AsyncGenerator
+from collections.abc import AsyncIterator
 from litestar import Litestar
 from litestar.testing import AsyncTestClient
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from app import app, Device, Base
+from app import app, encryption_helper
 import pytest
 import pytest_asyncio
 
 
-DATABASE_URL = 'sqlite+aiosqlite:///:memory:'
-engine = create_async_engine(DATABASE_URL)
-AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
-
-@pytest_asyncio.fixture(scope='function')
-async def test_transaction() -> AsyncGenerator[AsyncSession, None]:
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)  # Create tables
-
-    async with AsyncSessionLocal() as session:
-        yield session
-
-app.dependencies['transaction'] = lambda: AsyncSessionLocal()
-
 TEST_CLIENT_ID = '1'
+TEST_SHARED_SECRET = b'\xbd\xb4\xe9\xf7\x91\xf3\x97\x90\xc1\x93i\xe2\xc9\x0b\xa3\x115\xac\xcb<\xae\x96\xd6\x16\x88\x18\xc8\xd9FRG?'
+encryption_helper.shared_secrets[TEST_CLIENT_ID] = TEST_SHARED_SECRET
 
 
 @pytest_asyncio.fixture(scope='function')
@@ -29,23 +16,10 @@ async def test_client() -> AsyncIterator[AsyncTestClient[Litestar]]:
     async with AsyncTestClient(app=app) as client:
         yield client
 
-async def create_device(session: AsyncSession, mac_address: str, username: str = 'j_doe', password: str = 'password', key: str = 'key', preferences: dict = None):
-    if not preferences:
-        preferences = {'preference_1': '1.0'}
-    device = Device(
-        mac_address=mac_address,
-        username=username,
-        password=password,
-        key=key,
-        preferences=preferences,
-    )
-    session.add(device)
-    await session.commit()
-
 @pytest.mark.asyncio
-async def test_get_devices(test_client: AsyncTestClient[Litestar], test_transaction: AsyncSession) -> None:
-    async with test_transaction as session:
-        await create_device(session, '00:11:22:33:44:55')
+async def test_get_devices(test_client: AsyncTestClient[Litestar]) -> None:
+    {'mac_address': '00:11:22:33:44:55', 'username': 'jdoe', 'password': 'password'}
+    await test_client.post('/register')
 
     response = await test_client.get(f'/devices?client_id={TEST_CLIENT_ID}')
     print(response)
